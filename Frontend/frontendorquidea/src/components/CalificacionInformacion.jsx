2115f3f053946937eade64adb2ec4bb0ccec727c
Cristian <crisandresortiz3228@gmail.com>
2025-06-20 21:27:01 -0500

    organice el error que tenia una informacion del body

--- Frontend/frontendorquidea/src/components/CalificacionInformacion.jsx
+++ Frontend/frontendorquidea/src/components/CalificacionInformacion.jsx
@@ -3,7 +3,7 @@ import "../assets/styles/Experiencia.css"
 
 export default function CalificacionInformacion({texto}){
     return (
-        <div className="recuadro-informacion">
+        <div className="recuadro-informacion-2">
             <h4 className="estilo-titulo">{texto}</h4>
         </div>
     )
