01d199bb7570234dfc8da0a7dd458d607a837fb0
Carolina681 <vergarac681@gmail.com>
2025-07-14 00:39:01 -0500

    Mejora del apartado 'Trabajdor', mayor accesibilidad y comprensión con los pedidos, confirmaciones y enlaces

--- Frontend/src/pages/PagesTrabajador/HistorialPedidos.jsx
+++ Frontend/src/pages/PagesTrabajador/HistorialPedidos.jsx
@@ -5,19 +5,28 @@ import { useNavigate } from "react-router-dom";
 import TablaBase from "../../components/TablaBase";
 import Boton from "../../components/Boton";
 
+
 export default function HistorialPedidos() {
   const navigate = useNavigate();
 
-  const columnas = ["Pedidos", "Cliente", "Hora y fecha"];
+  const columnas = ["Pedidos", "Cliente", "Hora y fecha", "Detalles pedido", "Detalles cliente"];
 
-  const datos = [
-    ["26263", "José Muñoz", "12:05:23 PM - 12/02/2025"],
-    ["34562", "Luisa Ortiz", "01:25:53 PM - 08/03/2025"],
-    ["598941", "María Cano", "03:53:08 PM - 20/05/2025"],
-    ["346757", "Daniela Arboleda", "06:38:08 PM - 25/06/2025"],
-    ["347274", "Camila Jimenez", "09:43:54 AM - 02/09/2025"],
+    /* ~~~~~~ Simulación de los pedidos acorde a la pestaña ListaPedidos ~~~~~~ */
+    const pedidos = [
+    { id: "26263", cliente: "Camila  Pérez", fecha: "12:05:23 PM - 12/02/2025", clienteId: "cliente1" },
+    { id: "34562", cliente: "Juan García", fecha: "01:25:53 PM - 08/03/2025", clienteId: "cliente2" },
+    { id: "598941", cliente: "Laura Ríos", fecha: "03:53:08 PM - 20/05/2025", clienteId: "cliente3" },
+    { id: "346757", cliente: "Pedro Torres", fecha: "06:38:08 PM - 25/06/2025", clienteId: "cliente4" },
   ];
 
+  const datos = pedidos.map((pedido) => [
+    pedido.id,
+    pedido.cliente,
+    pedido.fecha,
+    <Boton texto="Ver pedido" onClick={() => navigate(`/DetallesPedido/${pedido.id}`)} />,
+    <Boton texto="Detalles del cliente" onClick={() => navigate(`/InfoCliente/${pedido.clienteId}`)} />
+  ]);
+  
   return (
     <div className="contenido">
       <h2 className="titulo">Historial de pedidos</h2>
@@ -25,6 +34,7 @@ export default function HistorialPedidos() {
 
       {/* Botón para volver al menú principal */}
       <Boton texto="Volver" onClick={() => navigate('/Inicio')} tipo="boton-personalizado" />
+      
     </div>
   );
 }
