83304bee991e4b1334026a979e6fac79f64b66d4
Carolina681 <vergarac681@gmail.com>
2025-07-21 20:26:45 -0500

    Diseño de footer, cambio de nombre de estilos por fallos en etiquetas globales, arreglo de daño de apartados de legalidades, más detalles e inicialización de limitaciones de apartados compartidos

--- Frontend/src/components/RutasLayouts.jsx
+++ Frontend/src/components/RutasLayouts.jsx
@@ -1,24 +1,28 @@
-
+// src/routes/RutaConLayout.jsx
 import React from 'react';
+import { useRol } from '../Context/RolContext';
+
 import MainLayout from '../layouts/MainLayout';
 import MainLayoutSinLogin from '../layouts/MainLayoutSinLogin';
 import AdminLayout from '../layouts/AdminLayout';
 import LayoutTrabajador from '../layouts/LayoutTrabajador';
 
-const RutaConLayout = ({ children }) => {
-    const rol = localStorage.getItem('rol');
+const RutasLayouts = ({ children }) => {
+  const { rol, cargando } = useRol();
 
-    switch (rol) {
+  if (cargando) return null; // o puedes mostrar <Loading />
+
+  switch (rol) {
     case 'cliente':
-        return <MainLayout>{children}</MainLayout>;
+      return <MainLayout>{children}</MainLayout>;
     case 'administrador':
-        return <AdminLayout>{children}</AdminLayout>;
+      return <AdminLayout>{children}</AdminLayout>;
     case 'trabajador':
-        return <LayoutTrabajador>{children}</LayoutTrabajador>;
-    case 'sinsegistrar':
+      return <LayoutTrabajador>{children}</LayoutTrabajador>;
+    case 'sinsegresar':
     default:
-        return <MainLayoutSinLogin>{children}</MainLayoutSinLogin>;
-    }
+      return <MainLayoutSinLogin>{children}</MainLayoutSinLogin>;
+  }
 };
 
-export default RutaConLayout;
+export default RutasLayouts;
--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -5,19 +5,22 @@ import './assets/styles/Global.css';
 
 import { CarritoProvider } from './Context/CarritoContext';
 import { FavoritosProvider } from './Context/FavoritosContext';
+import { RolProvider } from './Context/RolContext'; 
 import { GoogleOAuthProvider } from '@react-oauth/google';
-import { BrowserRouter } from 'react-router-dom'; // ✅ Importante
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId="119440441788-nilibr4jvgeu1gkvbu81v32c5i53cu12.apps.googleusercontent.com">
-      <BrowserRouter> {/* ✅ Aquí */}
-        <CarritoProvider>
-          <FavoritosProvider>
-            <AppRouter />
-          </FavoritosProvider>
-        </CarritoProvider>
+      <BrowserRouter>
+        <RolProvider> 
+          <CarritoProvider>
+            <FavoritosProvider>
+              <AppRouter />
+            </FavoritosProvider>
+          </CarritoProvider>
+        </RolProvider>
       </BrowserRouter>
     </GoogleOAuthProvider>
   </React.StrictMode>
