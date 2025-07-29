a272e80479fa179d033029d4a337e748837e0927
Cristian <crisandresortiz3228@gmail.com>
2025-07-29 07:03:30 -0500

    se realizo los ajustes en alguno de los errores que se estaba presentesentado en el apartado de favoritos con la autenticacion, tambien se comenzo el apartado del trabajador con la logica de la faactura que esta en el backend

--- Backend/trabajador/urls.py
+++ Backend/trabajador/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import listar_factura
+from .views import listar_factura, listar_pedidos
 from . import views
-
 
 urlpatterns = [
     #vista para traer todas las facturas
-    path('facturas/<int:id_factura>/', views.listar_factura, name='obtener_factura'),
+    path('listas_facturas/', views.listar_factura, name='lista_facturas'),
+    path('facturas/<int:id_factura>/pedidos/', listar_pedidos, name='listar_pedidos'),
 ]
-
