a1a113f4609989e5134121f59d9b2cfef4b0d158
Cristian <crisandresortiz3228@gmail.com>
2025-08-18 16:15:58 -0500

    se termino de organizar las rutas y se realizo algunos ajustes en el formulario, se agrego al boton de cancelar factura para borrar los productos y realicen la redireccion

--- Frontend/src/components/VentanaCookies.jsx
+++ Frontend/src/components/VentanaCookies.jsx
@@ -1,13 +1,28 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/AccedeAqui.css'; // ~~~~~~ Estilos del banner ~~~~~~
-
-/* ~~~~~~ Icono de cookies ~~~~~~ */
 import { BiSolidCookie } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 /* ~~~~~~ Componente funcional para el banner de cookies ~~~~~~ */
 export default function VentanaCookies({ onAceptar, onRechazar }) {
-    return (
+  const [visible, setVisible] = useState(true);
+  const navigate = useNavigate();
+
+  const handleAceptar = () => {
+    if (onAceptar) onAceptar();
+    setVisible(false); // 🔥 Cierra el banner
+  };
+
+
+  const handleLinkClick = (e) => {
+    e.preventDefault(); // Evita la navegación por defecto
+    setVisible(false); // 🔥 Cierra el banner
+    navigate("/PoliticaCo");
+  };
+
+  if (!visible) return null; // Si no está visible, no se muestra nada
+
+  return (
     <div className="FondoCookies">{/* ~~~~~~ Fondo del banner en la parte inferior ~~~~~~ */}
       <div className="VentanaC">{/* ~~~~~~ Contenedor del contenido ~~~~~~ */}
         
@@ -17,17 +32,20 @@ export default function VentanaCookies({ onAceptar, onRechazar }) {
         {/* ~~~~~~ Mensaje informativo ~~~~~~ */}
         <h1 className='EnfoqueCookies'>Utilizamos cookies</h1>
         <p className='InfoCookies'>
-            Usamos cookies para mejorar tu experiencia de navegación en nuestra web, 
-            para mostrarte contenidos personalizados y analizar el tráfico de nuestra web. 
-            <a href='/PoliticaCookies'><strong className='EnfoqueCookies'> Ver política de cookies.</strong></a>
+          Usamos cookies para mejorar tu experiencia de navegación en nuestra web, 
+          para mostrarte contenidos personalizados y analizar el tráfico de nuestra web. 
+          <a href="/PoliticaCo" onClick={handleLinkClick}>
+            <strong className='EnfoqueCookies'> Ver política de cookies.</strong>
+          </a>
         </p>
 
         {/* ~~~~~~ Botones de acción ~~~~~~ */}
         <div className="BotonesCookies">
-            <button className='Aceptar' onClick={onAceptar}>Aceptar</button>
-            <button className='Rechazar' onClick={onRechazar}>Rechazar</button>
+          <button className='Aceptar' onClick={handleAceptar}>Aceptar</button>
         </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
+
+
