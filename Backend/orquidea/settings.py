2d32b0f3d69facdceb1940d6649a6a704450c93e
Cristian <crisandresortiz3228@gmail.com>
2025-08-27 12:08:58 -0500

    agregando al app orquidea al settings

--- Backend/orquidea/settings.py
+++ Backend/orquidea/settings.py
@@ -94,6 +94,7 @@ INSTALLED_APPS = [
     # este es el apartado de apps, cada app creada se debe agregar en este apartado 
     #para que django pueda acceder a las carpetas de la aplicacion
     'administrador.apps.AdministradorConfig',
+    'orquidea' #app principal
     'carrito',
     'usuarios',
     'trabajador'
