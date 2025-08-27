20411287d315be56e1705da38329c82185c5aadc
Cristian <crisandresortiz3228@gmail.com>
2025-08-27 17:31:35 -0500

    organizando rutas

--- Frontend/src/api/axiosconfig.js
+++ Frontend/src/api/axiosconfig.js
@@ -2,12 +2,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/',
 });
 
 instance.interceptors.request.use(
   (config) => {
-    const token = sessionStorage.getItem('access'); // Cambiado de localStorage a sessionStorage
+    const token = sessionStorage.getItem('access');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
--- Frontend/src/api/usuario.js
+++ Frontend/src/api/usuario.js
@@ -1,8 +1,9 @@
-import axios from 'axios';
+// services/usuarioService.js
+import api from './api/axiosconfig';  // <-- importamos la instancia configurada
 
 export const obtenerUsuarioPorId = async (id_usuario) => {
   try {
-    const response = await axios.get(`/api/usuarios/${id_usuario}/`);
+    const response = await api.get(`usuarios/${id_usuario}/`);
     return response.data;
   } catch (error) {
     console.error('Error al obtener datos del usuario:', error);
